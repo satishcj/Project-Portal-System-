8a4bf6707e6674cd8acc63de9aebb72114d94d39
chiragR1006 <32605614+chiragR1006@users.noreply.github.com>
2017-11-02 22:24:13 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -4,10 +4,9 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<!DOCTYPE html>
 <html>
     <head>
-        <title>Railway Reservation</title>
+        <title>Project Portal System</title>
         <link rel="stylesheet" type="text/css" href="style.css">
     </head>
 <body>
@@ -15,7 +14,7 @@ and open the template in the editor.
     <h1>Project Portal System</h1>
     <a href="#" class="guest" >Guest Login</a> |&nbsp;
     <a href="#"> Forget password</a>
-    <form action="#" method="post">
+    <form action="studentHome.php" method="post">
     <div class="container">
         <h2>Login Form</h2>
         <label><b>Username</b></label>
@@ -25,8 +24,8 @@ and open the template in the editor.
     <input type="password" placeholder="Enter Password" name="psw" required>
         
     <button type="submit" class="button">Login</button>
-    <input type="button"  class="button" onclick="location.href='studentRegistration.php';"value="Register">
-
+    <input type="button"  class="button" onclick="location.href='studentRegistration.php';"value="Student Register">
+    <input type="button"  class="button" onclick="location.href='facultyRegistration.php';"value="Faculty Register">
   </div>
 </form>
 
